a4e8c4a6cfdd58a0026ce94de984f9fd43cb7801
alepa1992 <70697412+alepa1992@users.noreply.github.com>
2020-09-06 22:06:14 -0300

    EjercicioIII v1.0

--- EjercicioIII/EjercicioIII/Program.cs
+++ EjercicioIII/EjercicioIII/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Security.Cryptography.X509Certificates;
 
 namespace EjercicioIII
 {
@@ -10,6 +12,51 @@ namespace EjercicioIII
     {
         static void Main(string[] args)
         {
+            string palabra;
+            string reordenada = "";
+            int longitud;
+
+            palabra = Validador.ValidarTexto("Por favor ingrese la palabra que desee saber si es palíndromo.");
+
+            longitud = palabra.Length;
+
+            for (int i = 1; i < longitud + 1; i++)
+            {
+                reordenada = String.Concat(reordenada, palabra.Substring(longitud - i, 1));
+            }
+
+            if (palabra == reordenada)
+            {
+                Console.WriteLine("La palabra " + palabra + " es palindromo.");
+            }
+            else
+            {
+                Console.WriteLine("La palabra " + palabra + " no es palindromo.");
+            }
+
+            Console.WriteLine(palabra);
+            Console.WriteLine(reordenada);
+            Console.ReadKey();
         }
+        public class Validador
+        {
+            public static string ValidarTexto(string msj)
+            {
+                string res = "";
+
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine(msj);
+                    Console.WriteLine("Recuerde que no se permiten valore nulos, espacios o numeros.");
+                    res = Console.ReadLine().ToUpper();
+
+                    Console.Clear();
+                } while (string.IsNullOrWhiteSpace(res) || Regex.IsMatch(res, @"^[0-9]+$"));
+
+                return res;
+            }
+        }
+
     }
 }
