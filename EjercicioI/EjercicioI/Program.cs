db85fd3288bf23033fb469b81626db8c37285ca0
alepa1992 <70697412+alepa1992@users.noreply.github.com>
2020-09-06 21:58:30 -0300

    EjercicioI v1.0

---
+++ EjercicioI/EjercicioI/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Numero de registro: 890297
+            for (int i = 1; i < 298; i++)
+            {
+                if (i % 3 != 0 && i % 5 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+                if (i % 3 == 0 && i % 5 != 0)
+                {
+                    Console.WriteLine(i + "- Foo");
+                }
+
+                if (i % 3 != 0 && i % 5 == 0)
+                {
+                    Console.WriteLine(i + "- Bar");
+                }
+
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine(i + "- FooBar");
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
